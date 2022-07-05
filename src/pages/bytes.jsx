8b249673a4411f18ecb4d2a31431a4537f5fe15f
Brian Ketelsen <bketelsen@gmail.com>
2022-07-05 11:11:46 -0400

    some padding for bytes

--- src/pages/bytes.jsx
+++ src/pages/bytes.jsx
@@ -22,7 +22,7 @@ export default function Blog({posts}) {
               <Icon icon={"theming"} className="hidden w-16 h-16 mx-auto lg:block" />
               </div>
       </Hero>
-    <main className="max-w-[52rem] mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 lg:max-w-6xl not-prose">
+    <main className="max-w-[52rem] mx-auto px-4 pb-28 pt-10 sm:px-6 md:px-8 xl:px-12 lg:max-w-6xl not-prose">
       <div className="relative sm:pb-12 sm:ml-[calc(2rem+1px)] md:ml-[calc(3.5rem+1px)] lg:ml-[max(calc(14.5rem+1px),calc(100%-48rem))]">
         <div className="hidden absolute top-3 bottom-0 right-full mr-7 md:mr-[3.25rem] w-px bg-slate-200 dark:bg-slate-800 sm:block" />
         <div className="space-y-16">
